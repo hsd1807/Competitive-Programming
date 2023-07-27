36520cde191c72099a67ce97ea65b03a74df9e0a
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-07-27 19:00:02 +0530

    Solved

---
+++ HackerRank/C/Students Marks Sum.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+//Complete the following function.
+
+int marks_summation(int* marks, int n, char gender) 
+{
+  int i,sum=0;
+  if(gender=='b')
+  {
+      for(i=0;i<n;i=i+2) sum=sum+marks[i];
+  }
+  else
+  {
+      for(i=1;i<n;i=i+2) sum=sum+marks[i];
+  }
+  return sum;
+}
+
+int main() {
+    int number_of_students;
+    char gender;
+    int sum;
+  
+    scanf("%d", &number_of_students);
+    int *marks = (int *) malloc(number_of_students * sizeof (int));
+ 
+    for (int student = 0; student < number_of_students; student++) {
+        scanf("%d", (marks + student));
+    }
+    
+    scanf(" %c", &gender);
+    sum = marks_summation(marks, number_of_students, gender);
+    printf("%d", sum);
+    free(marks);
+ 
+    return 0;
+}
