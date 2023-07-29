9e0990a366456cc7d73ff76c0f0f4085478e97f7
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-07-29 18:36:50 +0530

    Solved

---
+++ HackerRank/C/Calculate the Nth term.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int find_nth_term(int n, int a, int b, int c) {
+  if(n==3) return c;
+  else return find_nth_term(n-1,b,c,a+b+c);
+}
+
+int main() {
+    int n, a, b, c;
+  
+    scanf("%d %d %d %d", &n, &a, &b, &c);
+    int ans = find_nth_term(n, a, b, c);
+ 
+    printf("%d", ans); 
+    return 0;
+}
