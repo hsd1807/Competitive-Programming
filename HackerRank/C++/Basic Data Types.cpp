d48cd74d11d83c331ff70c57afc322dddc7cf018
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-08-08 16:47:03 +0530

    Solved

---
+++ HackerRank/C++/Basic Data Types.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() 
+{
+    int a; long b; char c; float d; double e;
+    cin>>a>>b>>c>>d>>e;
+    cout<<a<<'\n'<<b<<'\n'<<c<<'\n';
+    printf("%.3f\n",d);
+    printf("%.9lf\n",e);
+    return 0;
+}
