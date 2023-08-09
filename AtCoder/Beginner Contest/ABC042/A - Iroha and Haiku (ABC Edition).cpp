8baedfa920d2765121a08bdd9c9cfe20698e89f9
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-08-09 21:08:24 +0530

    Upsolved

--- AtCoder/Beginner Contest/ABC042/A - Iroha and Haiku (ABC Edition).cpp
+++ AtCoder/Beginner Contest/ABC042/A - Iroha and Haiku (ABC Edition).cpp
@@ -1,18 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 int main()
 {
 ios::sync_with_stdio(false);
   cin.tie(0);
   int a,b,c;
-  int five=2,seven=1;
   cin>>a>>b>>c;
 
-  if(a==5) five--; else if(a==7) seven--;
-  if(b==5) five--; else if(b==7) seven--;
-  if(c==5) five--; else if(c==7) seven--;
-
-  if(five==0 && seven==0) cout<<"YES";
-  else cout<<"NO";
+  cout<<((a*b*c==175 && a+b+c==17)?"YES" : "NO");
   return 0;
 }
