1862938eef73887bae056fcb197e0971218ef20a
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-08-10 18:36:07 +0530

    Solved

---
+++ AtCoder/Beginner Contest/ABC045/A - Trapezoids.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int a,b,h;
+    cin>>a>>b>>h;
+    cout<<(a+b)*h/2;
+    return 0;
+}
