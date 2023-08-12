c64ffac9df503543f64c0b95067dc80f2d42f8d5
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-08-12 21:27:26 +0530

    Solved

---
+++ AtCoder/Beginner Contest/ABC048/A - AtCoder *** Contest.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  char a[8],b[101],c[9];
+  cin>>a>>b>>c;
+  cout<<'A'<<b[0]<<'C';
+  return 0;
+}
