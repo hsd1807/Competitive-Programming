51212f44ebfea307dc14af8304d42d3ffe29186e
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-08-13 09:45:46 +0530

    Solved

---
+++ AtCoder/Beginner Contest/ABC052/A - Two Rectangles.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(0);cout.tie(0);
+  int a,b,c,d;
+  cin>>a>>b>>c>>d;
+  cout<<(a*b>=c*d?a*b:c*d);
+  return 0;
+}
