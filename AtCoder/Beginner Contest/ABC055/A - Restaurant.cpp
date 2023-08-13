ca48fb87e8eff4170b724f89273f96419d91f951
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-08-13 09:48:58 +0530

    Solved

---
+++ AtCoder/Beginner Contest/ABC055/A - Restaurant.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(0);cout.tie(0);
+  int n;
+  cin>>n;
+  cout<<(n*800-(n/15)*200);
+  return 0;
+}
