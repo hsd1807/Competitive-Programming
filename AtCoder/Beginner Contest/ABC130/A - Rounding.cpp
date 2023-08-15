1d368ecb427243047cf518447a269b3c2bc4b7db
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-08-15 21:06:15 +0530

    Solved

---
+++ AtCoder/Beginner Contest/ABC130/A - Rounding.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(0);cout.tie(0);
+  int x,a;
+  cin>>x>>a;
+  cout<<(x<a?0:10);
+  return 0;
+}
