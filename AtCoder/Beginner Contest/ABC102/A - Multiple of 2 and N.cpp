028981c7528c73eeba0b045f194bd1d170294784
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-08-16 18:40:22 +0530

    Solved

---
+++ AtCoder/Beginner Contest/ABC102/A - Multiple of 2 and N.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    int n;
+    cin>>n;
+    cout<<(n%2==0?n:n*2);
+    return 0;
+}
