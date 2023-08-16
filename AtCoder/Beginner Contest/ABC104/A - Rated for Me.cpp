26e61c7b665cb31def607fef625d60311c824736
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-08-16 18:43:49 +0530

    Solved

---
+++ AtCoder/Beginner Contest/ABC104/A - Rated for Me.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    int r;
+    cin>>r;
+    cout<<(r<1200?"ABC":r<2800?"ARC":"AGC");
+    return 0;
+}
