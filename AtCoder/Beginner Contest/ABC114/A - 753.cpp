867b0962a945e4780c66120014db4dfc38cd83ba
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-08-17 15:17:10 +0530

    Solved

---
+++ AtCoder/Beginner Contest/ABC114/A - 753.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    int x;
+    cin>>x;
+    cout<<(x==7 || x==5 || x==3?"YES":"NO");
+    return 0;
+}
