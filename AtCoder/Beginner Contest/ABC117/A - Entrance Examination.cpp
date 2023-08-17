fdb66470ebd758349390d1542e78edec567a7aec
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-08-17 15:19:50 +0530

    Solved

---
+++ AtCoder/Beginner Contest/ABC117/A - Entrance Examination.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    int t; float x;
+    cin>>t>>x;
+    cout<<t/x;
+    return 0;
+}
