b61e961bb078ed30e18a63a2d99abf5458da23a4
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-08-17 15:23:12 +0530

    Solved

---
+++ AtCoder/Beginner Contest/ABC121/A - White Cells.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    int H,W,h,w;
+    cin>>H>>W>>h>>w;
+    cout<<(H-h)*(W-w);
+    return 0;
+}
