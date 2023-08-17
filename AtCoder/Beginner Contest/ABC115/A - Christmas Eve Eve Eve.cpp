d3abbdad77afba06898436aa76064a7a3d2cd24b
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-08-17 15:18:10 +0530

    Solved

---
+++ AtCoder/Beginner Contest/ABC115/A - Christmas Eve Eve Eve.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    int d;
+    cin>>d;
+    cout<<"Christmas ";
+    while(d<25)
+    {
+        cout<<"Eve ";
+        d++;
+    }
+    return 0;
+}
