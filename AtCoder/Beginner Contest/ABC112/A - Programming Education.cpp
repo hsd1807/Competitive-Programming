d9001ea097cba7e38570d208ee9ad0d6bceac29a
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-08-17 15:14:43 +0530

    Solved

---
+++ AtCoder/Beginner Contest/ABC112/A - Programming Education.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    int n;
+    cin>>n;
+    if(n==1) cout<<"Hello World";
+    else {int a,b; cin>>a>>b; cout<<a+b;}
+    return 0;
+}
