003122eabe992d6c8217fdd32032a3fe44f60746
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-08-17 15:08:55 +0530

    Solved

---
+++ AtCoder/Beginner Contest/ABC107/A - Train.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    int n,i;
+    cin>>n>>i;
+    cout<<n-i+1;
+    return 0;
+}
