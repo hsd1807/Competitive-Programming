e788c0a0e993cdd38d500d5bae0218c1ca03673b
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-08-19 21:24:20 +0530

    Solved

---
+++ AtCoder/Beginner Contest/ABC056/A - HonestOrDishonest.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(0);cout.tie(0);
+  char a,t;
+  cin>>a>>t;
+  cout<<(a=='H'?t:t=='H'?'D':'H');
+  return 0;
+}
