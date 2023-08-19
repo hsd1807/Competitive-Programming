34d7735eed42a34aa8e57e81bdbbbb2a0a6d04dc
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-08-19 21:19:28 +0530

    Solved

---
+++ AtCoder/Beginner Contest/ABC136/A - Transfer.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    int a,b,c;
+    cin>>a>>b>>c;
+    cout<<(c-(a-b)<0?0:c-a+b);
+    return 0;
+}
