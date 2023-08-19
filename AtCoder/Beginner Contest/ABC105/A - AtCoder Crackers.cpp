aae76cccdca2c0efe724564b20ff0f56cf54af53
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-08-19 21:25:13 +0530

    Solved

---
+++ AtCoder/Beginner Contest/ABC105/A - AtCoder Crackers.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    int n,k;
+    cin>>n>>k;
+    cout<<(n%k==0?0:1);
+    return 0;
+}
