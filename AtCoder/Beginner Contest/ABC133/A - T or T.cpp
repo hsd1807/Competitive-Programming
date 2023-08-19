0ec14b213e5233d12b140b8573a331e00752c7a5
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-08-19 21:21:47 +0530

    Solved

---
+++ AtCoder/Beginner Contest/ABC133/A - T or T.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    int n,a,b;
+    cin>>n>>a>>b;
+    cout<<min(n*a,b);
+    return 0;
+}
