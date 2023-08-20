4470da384455a910c0fad7dc41ea5c66fe46baa4
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-08-20 21:30:14 +0530

    Solved

---
+++ AtCoder/Beginner Contest/ABC138/A - Red or Not.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    int a;
+    string s;
+    cin>>a>>s;
+    cout<<(a<3200?"red":s);
+    return 0;
+}
