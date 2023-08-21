eca885a9b6f69e4db8ece59c5b3bc6b5f69d12f7
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-08-21 18:50:37 +0530

    Solved

---
+++ AtCoder/Beginner Contest/ABC147/A - Blackjack.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    int a1,a2,a3;
+    cin>>a1>>a2>>a3;
+    cout<<(a1+a2+a3<22?"win":"bust");
+    return 0;
+}
