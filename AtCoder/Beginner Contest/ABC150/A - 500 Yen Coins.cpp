cd237991afe4fd7e4d213cd55765359356410e40
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-08-21 21:33:21 +0530

    Solved

---
+++ AtCoder/Beginner Contest/ABC150/A - 500 Yen Coins.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    int k,x;
+    cin>>k>>x;
+    cout<<(k*500>=x?"Yes":"No");
+    return 0;
+}
