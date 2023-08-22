82e28d9049d92e0eb47821a3bee6dac12238f486
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-08-22 10:25:43 +0530

    Solved

---
+++ AtCoder/Beginner Contest/ABC063/A - Restricted.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    int a,b;
+    cin>>a>>b;
+    if(a+b<10) cout<<a+b;
+    else cout<<"error";
+    return 0;
+}
