7a30fde2b797e29a2b32750177e8b137d61b7be5
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-08-23 10:24:32 +0530

    Solved

---
+++ AtCoder/Beginner Contest/ABC076/A - Rating Goal.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    int r,g;
+    cin>>r>>g;
+    cout<<g*2-r;
+    return 0;
+}
