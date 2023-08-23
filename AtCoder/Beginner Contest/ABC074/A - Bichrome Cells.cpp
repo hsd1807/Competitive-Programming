a736a1804c21a6d33464d68bd6569e491640f87d
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-08-23 10:23:08 +0530

    Solved

---
+++ AtCoder/Beginner Contest/ABC074/A - Bichrome Cells.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    int n,a;
+    cin>>n>>a;
+    cout<<n*n-a;
+    return 0;
+}
