cc598e1d247282316662866142b98fd8cb868b2e
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-08-24 09:57:35 +0530

    Solved

---
+++ AtCoder/Beginner Contest/ABC087/A - Buying Sweets.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    int x,a,b;
+    cin>>x>>a>>b;
+    cout<<(x-a)%b;
+    return 0;
+}
