027595074518fb681df208390d91b453e61ccd58
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-08-24 09:59:03 +0530

    Solved

---
+++ AtCoder/Beginner Contest/ABC085/A - Already 2018.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    string s;
+    cin>>s;
+    s[3]='8';
+    cout<<s;
+    return 0;
+}
