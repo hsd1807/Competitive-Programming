99d1c5086296440b70ac96aa2a5d6f716f903d2c
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-08-24 09:59:53 +0530

    Solved

---
+++ AtCoder/Beginner Contest/ABC084/A - New Year.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    int m;
+    cin>>m;
+    cout<<48-m;
+    return 0;
+}
