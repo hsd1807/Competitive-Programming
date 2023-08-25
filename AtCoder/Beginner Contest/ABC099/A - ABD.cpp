38471de77d6bbd1e1cabc2b5d4941aab4b682465
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-08-25 11:52:15 +0530

    Solved

---
+++ AtCoder/Beginner Contest/ABC099/A - ABD.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    int n;
+    cin>>n;
+    cout<<(n>999?"ABD":"ABC");
+    return 0;
+}
