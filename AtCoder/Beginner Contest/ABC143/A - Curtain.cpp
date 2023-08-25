bc22fcd2d5a309b77393c3d807d76d79f8b8cba8
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-08-25 15:03:10 +0530

    Solved

---
+++ AtCoder/Beginner Contest/ABC143/A - Curtain.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    int a,b;
+    cin>>a>>b;
+    cout<<(b*2>=a?0:a-b*2);
+    return 0;
+}
