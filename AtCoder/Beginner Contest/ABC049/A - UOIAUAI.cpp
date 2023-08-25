7509d0aa075f94b20f07b972d0e34683dd0000de
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-08-25 15:57:22 +0530

    Upsolved

--- AtCoder/Beginner Contest/ABC049/A - UOIAUAI.cpp
+++ AtCoder/Beginner Contest/ABC049/A - UOIAUAI.cpp
@@ -2,10 +2,12 @@
 using namespace std;
 int main()
 {
-  ios::sync_with_stdio(false);
-  cin.tie(0);
-  char c;
-  cin>>c;
-  cout<<(c=='a' || c=='e' || c=='i' || c=='o' || c=='u'?"vowel":"consonant");
-  return 0;
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    char c ;
+    cin>>c ;
+    const string vowel = "aeiou";
+    if(vowel.find(c) != string:: npos) cout<<"vowel";
+    else cout<<"consonant";
+    return 0;
 }
