6ab94b68c53a9455a67a2b594090b610885f510e
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-08-25 11:55:45 +0530

    Solved

---
+++ AtCoder/Beginner Contest/ABC090/A - Diagonal String.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    string s[3];
+    cin>>s[0]>>s[1]>>s[2];
+    cout<<s[0][0]<<s[1][1]<<s[2][2];
+    return 0;
+}
