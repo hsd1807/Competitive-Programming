a40db6a4995a08566e31f40d2b6151d8d8273dee
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-08-27 20:51:11 +0530

    Solved

---
+++ AtCoder/Beginner Contest/ABC163/A - Circle Pond.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    int r;
+    cin>>r;
+    cout<<2*3.14159265359*r;
+    return 0;
+}
