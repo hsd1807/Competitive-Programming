971763cc445f467780eaf519e6b17732100dce43
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-08-27 20:52:58 +0530

    Solved

---
+++ AtCoder/Beginner Contest/ABC171/A - αlphabet.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    char a;
+    cin>>a;
+    cout<<(a<97?"A":"a");
+    return 0;
+}
