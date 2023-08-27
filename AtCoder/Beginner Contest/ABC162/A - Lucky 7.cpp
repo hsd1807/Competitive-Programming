3cc2747fe9370482dbabe81ff780e27ede825823
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-08-27 20:35:19 +0530

    Solved

---
+++ AtCoder/Beginner Contest/ABC162/A - Lucky 7.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    string n;
+    cin>>n;
+    if(n.find('7')!=string::npos) cout<<"Yes";
+    else cout<<"No";
+    return 0;
+}
