756e3c5d920afc2a8712edfeb4238700a8949cf9
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-08-28 21:30:16 +0530

    Solved

---
+++ AtCoder/Beginner Contest/ABC177/A - Don't be late.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    int d,t,s;
+    cin>>d>>t>>s;
+    cout<<(d<=s*t?"Yes":"No");
+    return 0;
+}
