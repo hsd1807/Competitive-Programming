0b6d670ea8232e6784452258739a3a288091bd57
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-08-29 09:41:55 +0530

    Solved

---
+++ AtCoder/Beginner Contest/ABC202/A - Three Dice.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    int a,b,c;
+    cin>>a>>b>>c;
+    cout<<21-(a+b+c);
+    return 0;
+}
