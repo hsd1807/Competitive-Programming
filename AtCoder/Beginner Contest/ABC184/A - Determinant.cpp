f594baa0e8e4e7ebca936ea4f00bd0de8c6b2da6
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-08-29 16:24:49 +0530

    Solved

---
+++ AtCoder/Beginner Contest/ABC184/A - Determinant.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    int a,b,c,d;
+    cin>>a>>b>>c>>d;
+    cout<<(a*d-b*c);
+    return 0;
+}
