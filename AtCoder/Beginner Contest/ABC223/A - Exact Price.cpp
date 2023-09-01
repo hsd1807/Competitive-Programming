64d3676fae71929a0bf2b1f2ed8cae23c9de943a
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-09-01 20:47:20 +0530

    Solved

---
+++ AtCoder/Beginner Contest/ABC223/A - Exact Price.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    int x;
+    cin>>x;
+    cout<<(x%100==0 && x!=0?"Yes":"No");
+    return 0;
+}
