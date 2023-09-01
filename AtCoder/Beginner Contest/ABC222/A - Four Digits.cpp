c21ab9b7de8751b5d1cfadf5ace962d00f6442cd
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-09-01 20:47:49 +0530

    Solved

---
+++ AtCoder/Beginner Contest/ABC222/A - Four Digits.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    string s;
+    cin>>s;
+    int len=s.size();
+    while(4-len++) cout<<0;
+    cout<<s;
+    return 0;
+}
