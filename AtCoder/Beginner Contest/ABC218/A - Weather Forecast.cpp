406e7a59c1e1df717178ce39a85a0bdec17202d0
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-09-02 16:10:05 +0530

    Solved

---
+++ AtCoder/Beginner Contest/ABC218/A - Weather Forecast.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    cout<<(s[n-1]=='o'?"Yes":"No");
+    return 0;
+}
