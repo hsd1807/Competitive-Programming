694e6757691d513dad44c6825109d553978083c3
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-09-02 15:40:52 +0530

    Solved

---
+++ AtCoder/Beginner Contest/ABC192/A - Star.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    int x;
+    cin>>x;
+    cout<<100-x%100;
+    return 0;
+}
