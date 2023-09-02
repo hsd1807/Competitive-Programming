12c0cd08634758d7c867350b66c5f06f305203ff
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-09-02 21:28:23 +0530

    Solved

---
+++ AtCoder/Beginner Contest/ABC281/A - Count Down.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    int n;
+    cin>>n;
+    n++;
+    while(n--) cout<<n<<'\n';
+    return 0;
+}
