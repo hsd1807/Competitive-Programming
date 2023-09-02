03019ab7d66f54b37c5d7314ec97bf190681a115
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-09-02 15:40:17 +0530

    Solved

---
+++ AtCoder/Beginner Contest/ABC193/A - Discount.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    int a,b;
+    cin>>a>>b;
+    cout<<(float)(a-b)*100/a;
+    return 0;
+}
