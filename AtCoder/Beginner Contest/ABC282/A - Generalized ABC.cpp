fc49777151c75c98d5e53dc74597fb32cf5e4dfd
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-09-02 21:32:37 +0530

    Solved

---
+++ AtCoder/Beginner Contest/ABC282/A - Generalized ABC.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    int n;
+    char a='A';
+    cin>>n;
+    for(int i=1;i<=n;i++) cout<<a++;
+    return 0;
+}
