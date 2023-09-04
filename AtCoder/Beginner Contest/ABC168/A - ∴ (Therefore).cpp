5b299bc58f2bf7e6f4ead20a98888d0f442c2e33
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-09-04 09:28:56 +0530

    Solved

---
+++ AtCoder/Beginner Contest/ABC168/A - ∴ (Therefore).cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    string p="pphbhhphph";
+    int n;
+    cin>>n;
+    cout<<p[n%10]<<"on";
+    return 0;
+}
