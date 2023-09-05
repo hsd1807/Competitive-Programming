f0f30d6a73dc942fbb9e8326b575f5ac56c97398
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-09-05 21:03:32 +0530

    Solved

---
+++ AtCoder/Beginner Contest/ABC034/A - Test.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    int x,y;
+    cin>>x>>y;
+    cout<<(x<y?"Better":"Worse");
+    return 0;
+}
