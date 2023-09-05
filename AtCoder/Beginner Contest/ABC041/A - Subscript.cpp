695c3d9d55b60919df44c3e1512e1181bf140ff0
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-09-05 21:08:18 +0530

    Solved

---
+++ AtCoder/Beginner Contest/ABC041/A - Subscript.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    string s;
+    int i;
+    cin>>s>>i;
+    cout<<s[i-1];
+    return 0;
+}
