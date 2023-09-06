c44da02babf04d69d752f2b08cfef6af0d0b9169
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-09-06 10:17:38 +0530

    Solved

---
+++ AtCoder/Beginner Contest/ABC226/A - Round decimals.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    int x,y;char p;
+    cin>>x>>p>>y;
+    cout<<(y>=500?x+1:x);
+    return 0;
+}
