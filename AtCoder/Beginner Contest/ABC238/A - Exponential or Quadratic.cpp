969107e93c565808182fe24dfebf2fe7e3726232
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-09-07 11:19:15 +0530

    Solved

---
+++ AtCoder/Beginner Contest/ABC238/A - Exponential or Quadratic.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    int n;
+    cin>>n;
+    cout<<(pow(2,n)>n*n?"Yes":"No");
+    return 0;
+}
