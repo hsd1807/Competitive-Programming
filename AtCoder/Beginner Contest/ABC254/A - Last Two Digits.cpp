d56ffc0b3943c9376e19e6518a61b85dfa9f21ef
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-09-10 21:17:39 +0530

    Solved

---
+++ AtCoder/Beginner Contest/ABC254/A - Last Two Digits.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    string s;
+    cin>>s;
+    cout<<s[1]<<s[2];
+    return 0;
+}
