896001cfa6ff776d6d36a3ba3c0f2e18a79ee6d2
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-09-10 21:09:20 +0530

    Accepted

---
+++ AtCoder/Beginner Contest/ABC154/B - I miss you... .cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    string s;
+    cin>>s;
+    for(int i=0;i<s.size();i++) s[i]='x';
+    cout<<s;
+    return 0;
+}
