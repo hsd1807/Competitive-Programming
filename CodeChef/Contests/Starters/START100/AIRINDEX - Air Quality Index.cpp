2e21a5aee83b199939da0226e9f81b9c6df50d85
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-09-14 10:25:53 +0530

    Solved

---
+++ CodeChef/Contests/Starters/START100/AIRINDEX - Air Quality Index.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    int x;
+    cin>>x;
+    cout<<(x<100?"YES":"NO");
+    return 0;
+}
