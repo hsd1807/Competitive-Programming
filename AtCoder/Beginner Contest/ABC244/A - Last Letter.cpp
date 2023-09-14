a8eac9c8c8b2e9f846c1a7493a9cbeddf116a509
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-09-14 10:20:01 +0530

    Solved

---
+++ AtCoder/Beginner Contest/ABC244/A - Last Letter.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    int n;string s;
+    cin>>n>>s;
+    cout<<s.back();
+    return 0;
+}
