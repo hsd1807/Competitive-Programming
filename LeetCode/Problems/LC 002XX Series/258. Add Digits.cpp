e3bc6fa559f4631c1fd157b42040d207dc0a9953
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-10-06 20:08:44 +0530

    Accepted

---
+++ LeetCode/Problems/LC 002XX Series/258. Add Digits.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        while(num>9)
+        {
+        int rem,ans=0;
+        while(num)
+        {
+            rem=num%10;
+            num/=10;
+            ans+=rem;
+        }
+        
+        num=ans;
+        }
+        return num;
+    }
+};
