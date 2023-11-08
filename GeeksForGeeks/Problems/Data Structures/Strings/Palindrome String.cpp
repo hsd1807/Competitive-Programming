20b1130e119028e3043d5f38aa2215a0cd33cada
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-11-08 21:02:18 +0530

    Solved

---
+++ GeeksForGeeks/Problems/Data Structures/Strings/Palindrome String.cpp
@@ -0,0 +1,13 @@
+class Solution{
+public:	
+	int isPalindrome(string S)
+	{
+	    int start=0,end=S.size()-1;
+	    while(start<end){
+	        if(S[start]!=S[end])
+            return 0;
+	        start++,end--;
+	    }
+	    return 1;
+	}
+};
