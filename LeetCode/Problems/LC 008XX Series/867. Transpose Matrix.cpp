0accd674ec953d4fe6d173144cdaae6850e34726
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-11-08 16:23:11 +0530

    Accepted

---
+++ LeetCode/Problems/LC 008XX Series/867. Transpose Matrix.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        int row=matrix.size();
+        int col=matrix[0].size();
+        vector<vector<int>> ans(col,vector<int>(row));
+
+        for(int i=0;i<row;i++)
+            for(int j=0;j<col;j++)
+                ans[j][i]=matrix[i][j];
+        return ans;
+    }
+};
