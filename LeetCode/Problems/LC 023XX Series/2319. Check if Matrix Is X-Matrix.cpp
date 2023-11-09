fac087d7fb4601433c3aeee7a1ae5bfad4a5ebcc
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-11-09 17:49:09 +0530

    Accepted

---
+++ LeetCode/Problems/LC 023XX Series/2319. Check if Matrix Is X-Matrix.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool checkXMatrix(vector<vector<int>>& grid) {
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid.size();j++){
+                if(i==j || j==grid.size()-i-1){
+                    if(grid[i][j]==0)
+                        return false;
+                }
+                else{
+                    if(grid[i][j]!=0)
+                        return false;
+                }
+            }
+        }
+        return true;
+    }
+};
