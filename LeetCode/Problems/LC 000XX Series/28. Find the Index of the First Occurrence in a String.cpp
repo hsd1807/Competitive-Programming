e4bf4410f6f09b4a439a323000d21c9c9e505b49
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-11-23 20:52:30 +0530

    Accepted

--- LeetCode/Problems/LC 000XX Series/28. Find the Index of the First Occurrence in a String.cpp
+++ LeetCode/Problems/LC 000XX Series/28. Find the Index of the First Occurrence in a String.cpp
@@ -1,35 +1,17 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        int n=needle.size(),h=haystack.size();
-        needle+=haystack;
-        vector<int> lps(n+h,0);
-        int prefix=0,suffix=1;
-        while(suffix<n){
-            if(needle[prefix]==needle[suffix]){
-                lps[suffix]=prefix+1;
-                prefix++,suffix++;
-            }
-            else{
-                if(prefix==0)
-                suffix++;
+        int n=haystack.size(),m=needle.size();
+        for(int i=0; i<=n-m;i++)
+        {
+            int first=i,second=0;
+            while(second<m){
+                if(haystack[first]!=needle[second])
+                    break;
                 else
-                prefix=lps[prefix-1];
+                    first++,second++;    
             }
-        }
-        prefix=0,suffix=n;
-        while(suffix<n+h){
-            if(needle[prefix]==needle[suffix]){
-                lps[suffix]=prefix+1;
-                if(lps[suffix]==n) return suffix-n-n+1;
-                prefix++,suffix++;
-            }
-            else{
-                if(prefix==0)
-                suffix++;
-                else
-                prefix=lps[prefix-1];
-            }
+            if(second==m) return i;
         }
         return -1;
     }
