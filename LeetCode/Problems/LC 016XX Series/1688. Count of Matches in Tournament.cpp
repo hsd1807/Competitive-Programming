bc18be7549395b94abae24bf25a9abe195bc11fe
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-12-06 15:26:59 +0530

    Accepted

--- LeetCode/Problems/LC 016XX Series/1688. Count of Matches in Tournament.cpp
+++ LeetCode/Problems/LC 016XX Series/1688. Count of Matches in Tournament.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int numberOfMatches(int n) {
+    int numberOfMatches(int n) { /* return n-1; -> Because in every match one team gets Eliminated. Therefore, it will take (n-1) matches to eliminate (n-1) teams, and get the Winner. */
         int matches=0;
         while(n!=1){
             if(n%2==0){
