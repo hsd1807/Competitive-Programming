f98789bc1e8a35e609499e87feef7e7aec1ff328
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-12-12 19:32:21 +0530

    Accepted

---
+++ LeetCode/Problems/LC 028XX Series/2828. Check if a String Is an Acronym of Words.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isAcronym(vector<string>& words, string s) {
+        if(words.size()!=s.size())
+            return false;
+        for(int i=0;i<words.size();i++){
+            if(s[i]!=words[i][0]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
