909ac552d6c91c7c10c17c5bdcfed14486cd4be2
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-12-12 21:43:44 +0530

    Accepted

---
+++ LeetCode/Problems/LC 007XX Series/709. To Lower Case.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    string toLowerCase(string s) {
+        for(int i=0;i<s.size();i++){
+            if(s[i]>='A' && s[i]<='Z')
+                s[i]+='a'-'A';
+        }
+        return s;
+    }
+};
