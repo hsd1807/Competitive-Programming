8750475053f79ee95d8f4fdf945e43e10d882546
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-12-31 20:55:02 +0530

    Accepted

---
+++ AtCoder/Beginner Contest/ABC252/A - ASCII Code.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    int n;
+    cin>>n;
+    char c=n;
+    cout<<c;
+    return 0;
+}
