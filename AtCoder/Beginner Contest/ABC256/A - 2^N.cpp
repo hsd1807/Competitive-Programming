a156c1b928e38a44b06b162cf4662189db92947d
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2023-12-31 20:53:49 +0530

    Accepted

---
+++ AtCoder/Beginner Contest/ABC256/A - 2^N.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    int n;
+    cin>>n;
+    int ans=pow(2,n);
+    cout<<ans;
+    return 0;
+}
