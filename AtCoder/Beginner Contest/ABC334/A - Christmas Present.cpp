4ded01cf2f7a53ae9741dc62558d349f5c684650
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2024-01-01 21:07:25 +0530

    Accepted

---
+++ AtCoder/Beginner Contest/ABC334/A - Christmas Present.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int b,g;
+    cin>>b>>g;
+    cout<<(b>g?"Bat":"Glove");
+    return 0;
+}
