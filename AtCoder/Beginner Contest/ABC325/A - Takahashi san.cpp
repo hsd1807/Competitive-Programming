a6f7107db26e3da7862ee841795a92bb6aacf89d
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2024-01-01 20:50:18 +0530

    Accepted

---
+++ AtCoder/Beginner Contest/ABC325/A - Takahashi san.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    string s,t;
+    cin>>s>>t;
+    t=" san";
+    cout<<s<<t;
+    return 0;
+}
