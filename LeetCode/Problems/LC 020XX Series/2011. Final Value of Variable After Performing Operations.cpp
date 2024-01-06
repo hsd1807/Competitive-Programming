8f1ef4c8e4be41b99d876c238e6d629ff83e491d
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2024-01-06 21:08:34 +0530

    Accepted

---
+++ LeetCode/Problems/LC 020XX Series/2011. Final Value of Variable After Performing Operations.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int x=0;
+        for(int i=0;i<operations.size();i++){
+            if(operations[i][1]=='+')
+                x++;
+            else
+                x--;
+        }
+        return x;
+    }
+};
