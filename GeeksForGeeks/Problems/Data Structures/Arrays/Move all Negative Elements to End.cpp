5224a9ddb6c0a41b0eb4478ad4218a103a4a3994
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2024-01-31 10:28:10 +0530

    Solved

--- GeeksForGeeks/Problems/Data Structures/Arrays/Move all Negative Elements to End.cpp
+++ GeeksForGeeks/Problems/Data Structures/Arrays/Move all Negative Elements to End.cpp
@@ -2,24 +2,20 @@ class Solution{
     public:
     void segregateElements(int arr[],int n)
     {
-        int p=0,positive[100000],N=0,negative[100000];
-        for(int i=0;i<n;i++){
-            if(arr[i]>-0)
-            {
-                positive[p]=arr[i];
-                p++;
-            }
+        int index = 0;
+        vector<int> positive, negative;
+        for (int i = 0; i < n; i++){
+            if (arr[i] < 0)
+                negative.push_back(arr[i]);
             else
-            {
-                negative[N]=arr[i];
-                N++;
-            }
+                positive.push_back(arr[i]);
         }
-      
-        for(int i=0;i<p;i++)
-        arr[i]=positive[i];
-        
-        for(int i=p;i<n;i++)
-        arr[i]=negative[i-p];
+        if (positive.size() == n || positive.size() == 0) return;
+        for (int i = 0; i < positive.size(); i++){
+            arr[index++] = positive[i];
+        }
+        for (int i = 0; i < negative.size(); i++){
+            arr[index++] = negative[i];
+        }
     }
 };
