ca32446c6c65bfe5b2c0f133515fb1fb62907e8b
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2024-02-08 10:18:18 +0530

    Solved

---
+++ GeeksForGeeks/Problems/Data Structures/Arrays/Rearrange Array Alternately.cpp
@@ -0,0 +1,28 @@
+class Solution{
+    public:
+    // This function wants you to modify the given input array and no need to return anything
+    // arr: input array
+    // n: size of array
+    //Function to rearrange  the array elements alternately.
+    void rearrange(long long *arr, int n) 
+    { 
+        int mx = INT_MIN;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > mx)
+                mx = arr[i];
+        }
+        mx++;
+        int start = 0, end = n - 1;
+        for (int i = 0; i < n; i++) {
+            if (i%2) {
+                arr[i] += mx * (arr[start++] % mx);
+            }
+            else {
+                arr[i] += mx * (arr[end--] % mx);
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            arr[i] = arr[i] / mx;
+        }
+    }
+};
