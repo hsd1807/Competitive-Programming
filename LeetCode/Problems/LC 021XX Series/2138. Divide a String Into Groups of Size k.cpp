fb68b42f16f03cdc664a99e20b2ac13f0512b52c
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2024-02-22 19:34:18 +0530

    Accepted

--- LeetCode/Problems/LC 021XX Series/2138. Divide a String Into Groups of Size k.cpp
+++ LeetCode/Problems/LC 021XX Series/2138. Divide a String Into Groups of Size k.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     vector<string> divideString(string s, int k, char fill) {
-        vector<string> ans;
-        int index = -1;
+        vector<string> ans; int index = -1;
         for (int i = 0; i < s.size(); i++) {
             if (i % k == 0) {
                 ans.push_back("");
