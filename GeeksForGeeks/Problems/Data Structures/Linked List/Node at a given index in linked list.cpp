9a6d3b8135810dd026440414419b83787f793128
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2024-03-06 15:51:13 +0530

    Solved

---
+++ GeeksForGeeks/Problems/Data Structures/Linked List/Node at a given index in linked list.cpp
@@ -0,0 +1,21 @@
+/* Print he nth node in the linked list Node is defined as 
+/* Link list node
+struct node
+{
+    int data;
+    struct node* next;
+    
+    node(int x){
+        data = x;
+        next = NULL;
+    }
+}; 
+*/
+
+// Should return data of node at given index. The function may assume that there are at least index+1 nodes in linked list.
+int GetNth(struct node* head, int index){
+    while (--index) {
+        head = head->next;
+    }
+    return head->data;
+}
