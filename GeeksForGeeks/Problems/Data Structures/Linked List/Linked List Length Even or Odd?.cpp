ccfca01dfdab43c152be2be240f8d6e270aed595
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2024-03-06 15:59:39 +0530

    Solved

---
+++ GeeksForGeeks/Problems/Data Structures/Linked List/Linked List Length Even or Odd?.cpp
@@ -0,0 +1,23 @@
+/*structure of a node of the linked list is as
+struct Node
+{
+	int data;
+	struct Node* next;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	}
+	
+};
+*/
+// Function should return 0 is length is even else return 1
+int isLengthEvenOrOdd(struct Node* head)
+{
+     int index = 0;
+     while (head) {
+         head = head->next;
+         index++;
+     }
+     return index % 2;
+}
