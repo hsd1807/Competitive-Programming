119e44707614d1364f91f3e0ecdb1734833910cd
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2024-04-14 09:27:25 +0530

    Accepted

---
+++ AtCoder/CODE FESTIVAL/2017/qual B/A - XXFESTIVAL.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	string s;
+	cin >> s;
+	cout << s.substr(0, s.size()-8) << '\n';
+	return 0;
+}
