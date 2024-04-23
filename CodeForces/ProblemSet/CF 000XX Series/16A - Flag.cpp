52c167ee2e6eb768f9e02a7f2d5d29aacc259f99
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2024-04-23 09:04:11 +0530

    Accepted

--- CodeForces/ProblemSet/CF 000XX Series/16A - Flag.cpp
+++ CodeForces/ProblemSet/CF 000XX Series/16A - Flag.cpp
@@ -1,33 +1,30 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+
 using namespace std;
 
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-    int n,m;
-    cin>>n>>m;
-    vector<string> flag(n);
-    for(int i=0;i<n;i++){
-        cin>>flag[i];
-    }
-    bool ok=true;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m-1;j++){
-            if(flag[i][j]!=flag[i][j+1]){
-                ok=false;
-                break;
-            }
-        }
-        if(ok&&i){
-            if(flag[i][0]==flag[i-1][0]){
-                ok=false;
-                break;
-            }
-        }
-        if(!ok) break;
-    }
-    cout<<(ok?"YES":"NO");
-    return 0;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n, m;
+	cin >> n >> m;
+	bool ok = true;
+	char prev = 'a';
+	for (int i = 0; i < n; i++) {
+		string s;
+		cin >> s;
+		for (int i = 1; i < m; i++) {
+			if (s[i] != s[i-1]) {
+				ok = false;
+				break;
+			}
+		}
+		if (!ok) break;
+		if (s[0] == prev) {
+			ok = false;
+			break;
+		}
+		prev = s[0];
+	}
+	cout << (ok ? "YES" : "NO") << '\n';
+	return 0;
 }
