27b7144df32017ed20cf982306406085e702f37b
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2024-05-25 21:00:39 +0530

    Accepted

--- CodeForces/ProblemSet/CF 000XX Series/16A - Flag.cpp
+++ CodeForces/ProblemSet/CF 000XX Series/16A - Flag.cpp
@@ -7,23 +7,24 @@ int main() {
 	cin.tie(0);
 	int n, m;
 	cin >> n >> m;
+	char pre = '$';
 	bool ok = true;
-	char prev = 'a';
 	for (int i = 0; i < n; i++) {
 		string s;
 		cin >> s;
-		for (int i = 1; i < m; i++) {
-			if (s[i] != s[i-1]) {
-				ok = false;
-				break;
+		if (pre != s[0]) {
+			for (int j = 1; j < m; j++) {
+				if (s[j] != s[j - 1]) {
+					ok = false;
+					break;
+				}
 			}
+			pre = s[0];
+		}
+		else {
+			ok = false;
 		}
 		if (!ok) break;
-		if (s[0] == prev) {
-			ok = false;
-			break;
-		}
-		prev = s[0];
 	}
 	cout << (ok ? "YES" : "NO") << '\n';
 	return 0;
