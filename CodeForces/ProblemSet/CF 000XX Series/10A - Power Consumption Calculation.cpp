f65d39bcd9e01789d5d9dd96767fbdc4005b3a52
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2024-05-25 20:58:51 +0530

    Accepted

--- CodeForces/ProblemSet/CF 000XX Series/10A - Power Consumption Calculation.cpp
+++ CodeForces/ProblemSet/CF 000XX Series/10A - Power Consumption Calculation.cpp
@@ -1,30 +1,30 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
-	int n, p1, p2, p3, t1, t2, power = 0, last = -1;
+	int n, p1, p2, p3, t1, t2;
 	cin >> n >> p1 >> p2 >> p3 >> t1 >> t2;
+	int power = 0, pre = 0;
 	while (n--) {
 		int l, r;
 		cin >> l >> r;
-		power += (r-l)*p1;
-		if(last != -1) {
-			int extra = l - last;
-			if(extra < t1) power += extra*p1;
+		power += (r - l) * p1;
+		if (pre) {
+			int time = l - pre;
+			if (time <= t1) {
+				power += p1 * time;
+			}
+			else if (time <= t1 + t2) {
+				power += p1 * t1 + p2 * (time - t1);
+			}
 			else {
-				power += t1*p1;
-				extra -= t1;
-				if (extra < t2) power += extra*p2;
-				else {
-					power += t2*p2;
-					extra -= t2;
-					power += extra*p3;
-				}
+				power += p1 * t1 + p2 * t2 + p3 * (time - t1 - t2);
 			}
 		}
-		last = r;
+		pre = r;
 	}
 	cout << power << '\n';
 	return 0;
