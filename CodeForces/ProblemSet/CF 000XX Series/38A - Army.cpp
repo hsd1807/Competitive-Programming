937474c0a1c444b2863ff1a3a942183d7c7b5810
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2024-05-26 21:29:37 +0530

    Accepted

--- CodeForces/ProblemSet/CF 000XX Series/38A - Army.cpp
+++ CodeForces/ProblemSet/CF 000XX Series/38A - Army.cpp
@@ -1,22 +1,22 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+
 using namespace std;
 
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-    int n;
-    cin>>n;
-    vector<int> ranks(n);
-    for(int i=0;i<n-1;i++){
-        cin>>ranks[i];
-    }
-    int a,b,years=0;
-    cin>>a>>b;
-    for(int i=a;i<b;i++){
-        years+=ranks[i-1];
-    }
-    cout<<years;
-    return 0;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n;
+	cin >> n;
+	vector<int> d(n);
+	for (int i = 1; i < n; i++) {
+		cin >> d[i];
+	}
+	int a, b;
+	cin >> a >> b;
+	int ans = 0;
+	for (int i = a; i < b; i++) {
+		ans += d[i];
+	}
+	cout << ans << '\n';
+	return 0;
 }
