e0156d31d00b608b255850a42995e0c6e51d08ab
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2024-05-29 18:40:47 +0530

    Accepted

--- CodeForces/ProblemSet/CF 000XX Series/50A - Domino Piling.cpp
+++ CodeForces/ProblemSet/CF 000XX Series/50A - Domino Piling.cpp
@@ -1,13 +1,12 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+
 using namespace std;
 
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-    int m,n;
-    cin>>m>>n;
-    cout<<(m*n%2?(m*n-1)/2:(m*n)/2);
-    return 0;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int m, n;
+	cin >> m >> n;
+	cout << m * n / 2 << '\n';
+	return 0;
 }
