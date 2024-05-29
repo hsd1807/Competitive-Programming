1151aa626b099cf95cae2963c78280f4e2e53ac5
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2024-05-29 18:45:05 +0530

    Accepted

--- CodeForces/ProblemSet/CF 000XX Series/46A - Ball Game.cpp
+++ CodeForces/ProblemSet/CF 000XX Series/46A - Ball Game.cpp
@@ -1,18 +1,16 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+
 using namespace std;
 
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(0);cout.tie(0);
-    int n;
-    cin>>n;
-    int pass=1,pos=1;
-    for(int i=1;i<n;i++){
-        pos+=pass;
-        pass++;
-        if(pos%n==0) cout<<n<<" ";
-        else cout<<pos%n<<" ";
-    }
-    return 0;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n;
+	cin >> n;
+	int start = 1, jump = 1;
+	for (int i = 1; i < n; i++) {
+		start = (start + jump++) % n;
+		cout << (start == 0 ? n : start) << " \n"[i == n - 1];
+	}
+	return 0;
 }
