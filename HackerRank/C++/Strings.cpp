a2507e468b830de28629937a74b5faac492b98be
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2024-06-11 21:04:03 +0530

    Solved

---
+++ HackerRank/C++/Strings.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	  string s1, s2;
+    cin >> s1 >> s2;
+    cout << s1.size() << ' ' << s2.size() << '\n';
+    cout << s1 + s2 << '\n';
+    swap(s1[0], s2[0]);
+    cout << s1 << ' ' << s2; 
+    return 0;
+}
