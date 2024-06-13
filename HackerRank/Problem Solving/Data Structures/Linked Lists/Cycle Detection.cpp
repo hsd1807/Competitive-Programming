0e769ee0c3428fc7baa318307fd14038ab6b0a21
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2024-06-13 17:09:17 +0530

    Accepted

---
+++ HackerRank/Problem Solving/Data Structures/Linked Lists/Cycle Detection.cpp
@@ -0,0 +1,148 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class SinglyLinkedListNode {
+    public:
+        int data;
+        SinglyLinkedListNode *next;
+
+        SinglyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+        }
+};
+
+class SinglyLinkedList {
+    public:
+        SinglyLinkedListNode *head;
+        SinglyLinkedListNode *tail;
+
+        SinglyLinkedList() {
+            this->head = nullptr;
+            this->tail = nullptr;
+        }
+
+        void insert_node(int node_data) {
+            SinglyLinkedListNode* node = new SinglyLinkedListNode(node_data);
+
+            if (!this->head) {
+                this->head = node;
+            } else {
+                this->tail->next = node;
+            }
+
+            this->tail = node;
+        }
+};
+
+void print_singly_linked_list(SinglyLinkedListNode* node, string sep, ofstream& fout) {
+    while (node) {
+        fout << node->data;
+
+        node = node->next;
+
+        if (node) {
+            fout << sep;
+        }
+    }
+}
+
+void free_singly_linked_list(SinglyLinkedListNode* node) {
+    while (node) {
+        SinglyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+// Complete the has_cycle function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+ /*
+ Brute Force Approach : Keep visiting every node, and keep storing its address in some data structure, and also when visitin a node, check the data structure if that node has been visited or not.
+ TC = O(n ^ 2), SC = O(n);
+ */
+ /*
+ Using the concept of Maps : Keep visiting every node, and keep storing its address in a map data sturcture, andh when visiting a node, then again, we will check the map, tha whether that node has been visited or not.
+ TC = O(N), SC = O(n);
+ */
+ /*
+ Using the slow and fast pointer : Then we will move slow pointer one node at a time and the fast pointer 2 nodes at a time. If the loop exists, the fast pointer will encounter the slow pointer at some node, else the fast pointer will get to a node which points to NULL ie No loop exists.
+ head -> 1 -> 2 -> 3 -> 4 -> 5 -> NULL;
+                s              f         
+ TC = O(N), SC = O(1)
+ */
+bool has_cycle(SinglyLinkedListNode* head) {
+    if (head == NULL || head->next == NULL) return false;
+    SinglyLinkedListNode* slow = head, *fast = head;
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast) {
+            return true;
+        }   
+    }
+    return false;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int tests;
+    cin >> tests;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int tests_itr = 0; tests_itr < tests; tests_itr++) {
+        int index;
+        cin >> index;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        SinglyLinkedList* llist = new SinglyLinkedList();
+
+        int llist_count;
+        cin >> llist_count;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        for (int i = 0; i < llist_count; i++) {
+            int llist_item;
+            cin >> llist_item;
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+            llist->insert_node(llist_item);
+        }
+      
+      	SinglyLinkedListNode* extra = new SinglyLinkedListNode(-1);
+      	SinglyLinkedListNode* temp = llist->head;
+      
+      	for (int i = 0; i < llist_count; i++) {
+            if (i == index) {
+          		extra = temp;
+            }
+          	
+          	if (i != llist_count-1) {
+          		temp = temp->next;
+            }
+        }
+      
+      	temp->next = extra;
+
+        bool result = has_cycle(llist->head);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
