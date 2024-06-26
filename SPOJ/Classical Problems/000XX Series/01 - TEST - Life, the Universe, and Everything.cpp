e05af386ee03977095b1d299bed8921ec0ad1335
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2024-06-26 21:57:03 +0530

    Accepted

---
+++ SPOJ/Classical Problems/000XX Series/01 - TEST - Life, the Universe, and Everything.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n;
+	cin >> n;
+	while (n != 42) {
+		cout << n << '\n';
+		cin >> n;
+	}
+	return 0;
+}
