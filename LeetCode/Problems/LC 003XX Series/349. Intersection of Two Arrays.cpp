59847e561e22b58fb9f3ddd469faf52ae85cd6e7
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2024-07-07 14:25:05 +0530

    Accepted

---
+++ LeetCode/Problems/LC 003XX Series/349. Intersection of Two Arrays.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> n1(1001, 0), n2(1001, 0), ans;
+        for (int i = 0; i < nums1.size(); i++) {
+            n1[nums1[i]]++;
+        }
+        for (int i = 0; i < nums2.size(); i++) {
+            n2[nums2[i]]++;
+        }
+        for (int i = 0; i < 1001; i++) {
+            if (n1[i] && n2[i]) {
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+};
