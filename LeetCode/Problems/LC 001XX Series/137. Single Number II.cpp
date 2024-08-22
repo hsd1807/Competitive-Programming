33d69a7c019fafc1f4efb1c44a42ee11ced9f038
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2024-08-22 13:41:49 +0530

    Accepted

--- LeetCode/Problems/LC 001XX Series/137. Single Number II.cpp
+++ LeetCode/Problems/LC 001XX Series/137. Single Number II.cpp
@@ -1,11 +1,18 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size()-1;i+=3){
-            if(nums[i]!=nums[i+1])
-                return nums[i];
+        vector<int> bits(32, 0);
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = 0; j < 32; j++) {
+                if (nums[i] & (1 << j))
+                    bits[j]++;
+            }
         }
-        return nums.back();
+        int ans = 0;
+        for (int i = 0; i < 32; i++) {
+            if (bits[i] % 3) 
+                ans = ans | (1 << i);
+        }
+        return ans;
     }
 };
